031b71cdc24937a909083270b8344bb29f5866f1
AmineHLub <Amino.bhl@gmail.com>
2021-10-05 12:13:39 +0100

    Add local storagefetch-and-print function

---
+++ src/getData.js
@@ -0,0 +1,19 @@
+console.log('hello');
+
+const ulWrap = document.querySelector('.score-list');
+
+let arrOfScores = [];
+
+const previousData = JSON.parse(localStorage.getItem('storedScores'));
+
+if (!previousData) {
+  localStorage.setItem('storedScores', JSON.stringify(arrOfScores));
+} else {
+  arrOfScores = previousData;
+}
+
+for (let i = 0; i < arrOfScores.length; i += 1) {
+  const newLi = document.createElement('li');
+  ulWrap.appendChild(newLi);
+  document.querySelectorAll('.score-list > li')[i].innerText = `${arrOfScores[i].name} : ${arrOfScores[i].value}`;
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './style.css';
+import getData from './getData.js';
+
+const Reloader = document.querySelector('.reload-btn');
+
+Reloader.addEventListener('click', () => {
+  window.location.reload();
+});
+
+getData();
