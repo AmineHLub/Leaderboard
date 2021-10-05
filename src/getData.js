dafb48ee112baea6b7af5c8c7840def2dbc779c8
AmineHLub <Amino.bhl@gmail.com>
2021-10-05 13:40:39 +0100

    Add Functionality: localstorage for input values and socrelist

--- src/getData.js
+++ src/getData.js
@@ -1,10 +1,14 @@
-console.log('hello');
-
 const ulWrap = document.querySelector('.score-list');
 
 let arrOfScores = [];
 
 const previousData = JSON.parse(localStorage.getItem('storedScores'));
+const previousInput = JSON.parse(localStorage.getItem('storedInput'));
+
+if (previousInput) {
+  [document.querySelector('.name-input').value,
+    document.querySelector('.score-input').value] = previousInput;
+}
 
 if (!previousData) {
   localStorage.setItem('storedScores', JSON.stringify(arrOfScores));
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import './style.css';
 import getData from './getData.js';
+import saveInput from './saveInput.js';
 
+document.querySelector('.name-input').addEventListener('change', saveInput);
+document.querySelector('.score-input').addEventListener('change', saveInput);
+
+getData();
 const Reloader = document.querySelector('.reload-btn');
 
 Reloader.addEventListener('click', () => {
   window.location.reload();
 });
-
-getData();
