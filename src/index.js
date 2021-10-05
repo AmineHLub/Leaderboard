cb96fb7ed475128bc05678d25b374ab6ecbfbb6c
AmineHLub <Amino.bhl@gmail.com>
2021-10-05 14:43:05 +0100

    Add functionality : Adding

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 import getData from './getData.js';
 import saveInput from './saveInput.js';
+import addScores from './addScores.js';
 
 document.querySelector('.name-input').addEventListener('change', saveInput);
 document.querySelector('.score-input').addEventListener('change', saveInput);
@@ -10,4 +11,5 @@ Reloader.addEventListener('click', () => {
   window.location.reload();
 });
 
+document.querySelector('.submit-btn').addEventListener('click', addScores);
 getData();
